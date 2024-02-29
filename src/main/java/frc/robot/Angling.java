00e93f3aeb6c55eb97bd9f3d123c77b2c135047f
Team-4585 <84157800+Team-4585@users.noreply.github.com>
2024-02-28 21:11:43 -0700

    Before Comp

--- src/main/java/frc/robot/Angling.java
+++ src/main/java/frc/robot/Angling.java
@@ -11,15 +11,19 @@ public class Angling extends RoboDevice{
   public Angling(){
     super("Angling Sub System");
     m_angleMotor = new BasicPID(WiringConnections.SHOOTER_ANGLE_CONTROLLER_ID);
+    angle = m_angleMotor.getPosition();
   }
 
   public void Initialize(){
     m_angleMotor = new BasicPID(WiringConnections.SHOOTER_ANGLE_CONTROLLER_ID);
+    angle = m_angleMotor.getPosition();
   }
 
   public void move(double speed){
     angle = angle - speed / 5;
     m_angleMotor.setRotations(angle);
+    System.out.println("Target: " + angle);
+    System.out.println("Current: " + m_angleMotor.getPosition());
   }
 
   @Override
--- src/main/java/frc/robot/huskylib/src/BasicPID.java
+++ src/main/java/frc/robot/huskylib/src/BasicPID.java
@@ -42,13 +42,13 @@ public class BasicPID extends RoboDevice{
     m_pidController.setFeedbackDevice(m_encoder);
 
     //Setting up PID values
-    kP = 0.1;
+    kP = 0.75;
     kI = 0;
     kD = 0;
     kIz = 0;
     kFF = 0;
-    kMaxOutput = 0.5;
-    kMinOutput = -0.5;
+    kMaxOutput = 0.75;
+    kMinOutput = -0.75;
     
     
     m_pidController.setP(kP);
